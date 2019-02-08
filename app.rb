19592831f4ced7cba7a3ca8df4ed79ae2dd79a08
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-02-08 18:54:59 -0500

    Skip the expression if invalid

--- app.rb
+++ app.rb
@@ -17,18 +17,18 @@ class RewardSystem < Sinatra::Base
       action = line[1].to_sym
       case action
       when :recommends
-        # check if line[2] is provided
+        next unless line[2]
         inviter, invitee = line[0], line[2] 
         customers[inviter] = customers[inviter] || Customer.new;
         customers[invitee] = customers[invitee] || Customer.new;
         customers[inviter].recommends(customers[invitee])
       when :accepts
         invitee = line[0]
+        next unless customers[invitee]
         customers[invitee].accepts
       end
     end
     customers.each { |key, value| customers[key] = value.point}
-  
     erb :result, locals: { customers: customers }
   end
 end
