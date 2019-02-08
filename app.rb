41d0eae7cd6e597e33e653fcba93a728b6569ba7
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-02-08 17:15:13 -0500

    Add routes and perform an action based on the route type

--- app.rb
+++ app.rb
@@ -7,7 +7,28 @@ class RewardSystem < Sinatra::Base
   set :public_folder, Proc.new { File.join(File.dirname(__FILE__), 'content') }
 
   get '/' do
-    erb :home, locals: { customers: "" }
+    erb :home
   end
   
+  post '/result' do
+    customers = {}
+    params[:document] && params[:document][:tempfile].read.each_line do |line|
+      line = line.gsub(/[^a-zA-Z ]/,'').strip.split(' ')
+      action = line[1].to_sym
+      case action
+      when :recommends
+        # check if line[2] is provided
+        inviter, invitee = line[0], line[2] 
+        customers[inviter] = customers[inviter] || Customer.new;
+        customers[invitee] = customers[invitee] || Customer.new;
+        customers[inviter].recommends(customers[invitee])
+      when :accepts
+        invitee = line[0]
+        customers[invitee].accepts
+      end
+    end
+    customers.each { |key, value| customers[key] = value.point}
+  
+    erb :result, locals: { customers: customers }
+  end
 end
