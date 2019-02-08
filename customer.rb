d4693e7e0c5cea257d2cc3601a52ed7ffa59d6aa
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-02-08 11:49:36 -0500

    Add customer class

---
+++ customer.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+class Customer
+  attr_accessor :inviter, :point, :level
+
+  def initialize
+    @point = 0
+    @level = 0
+  end
+
+  def recommends(invitee)
+    invitee.inviter = self if invitee.inviter.nil?  
+  end  
+
+  def reward(customer)
+    customer.point += ( 1.0 / 2 ) ** customer.level
+    customer.level += 1
+    return if customer.inviter.nil?
+    reward(customer.inviter)
+  end
+
+  def accepts
+    return if @inviter.nil?
+    reward @inviter
+  end
+end
